ddb9cd09d250547c10e7b1d0a01df3199a308a4a
Stuart Thompson <stuart@stuartthompson.net>
2012-12-03 08:38:14 -0800

    Adds Icon-CSS-Generator to Research

---
+++ Development/CSharp/Icon-CSS-Generator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace IconCSSGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: icssgen <iconDirectoryPath> <projectSubFolderName> <dimension>");
+                return;
+            }
+
+            string iconDirectoryPath = args[0];
+            string projectSubFolderName = args[1];
+            string dimension = args[2];
+
+            // Open the directory containing the icons
+            using (FileStream cssFileStream = new FileStream("icons.css", FileMode.Create))
+            {
+                using (StreamWriter sw = new StreamWriter(cssFileStream))
+                {
+                    string[] iconFiles = Directory.GetFiles(iconDirectoryPath);
+                    foreach (string iconFile in iconFiles)
+                    {
+                        string iconFileName = iconFile.Replace(iconDirectoryPath + "\\", "");
+                        string iconName = iconFileName.Substring(0, iconFileName.IndexOf('-'));
+                        sw.WriteLine(String.Format(".icon-{0}-{2}x{2} {{ display: inline-block; width: {2}px; height: {2}px; background-image: url('../images/icons/{1}/{0}-{2}x{2}.png'); }}", iconName, projectSubFolderName, dimension));
+                    }
+                }
+            }
+            
+        }
+    }
+}
---
+++ Development/CSharp/Icon-CSS-Generator/README.md
@@ -0,0 +1,4 @@
+Icon-CSS-Generator
+==================
+
+Simple console application used to generate css for a set of icon files.
